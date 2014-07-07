237c0e3bd8334a6adc61fd21ad094168d1a9b0b7
Brandon D'Souza <brandondsz26@gmail.com>
2014-07-07 16:49:35 +0530

    Added Logon and Register

---
+++ EmployeeReview/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using EmployeeReview.Models;
+
+namespace EmployeeReview.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                MembershipCreateStatus createStatus;
+                Membership.CreateUser(model.UserID, model.Fname, model.Lname, model.Email, model.Password, model.IsActive,out createStatus);
+
+                if (createStatus == MembershipCreateStatus.Success)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserID, false );
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Some error occured.");
+                }
+            }
+            return View(model);
+
+        }
+    }
+}
---
+++ EmployeeReview/Models/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace EmployeeReview.Models
+{
+    public class Account
+    {
+    }
+    public class LogOnModel
+    {
+        [Required]
+        [Display(Name = "User ID")]
+        public string UserID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "User ID")]
+        public string UserID { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string Fname { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lname { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [System.Web.Mvc.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
