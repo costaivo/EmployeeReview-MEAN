b50048713c2878d58c86a8a5c84ae89658876378
Gaurav Borkar <gaurav@creativecapsule.com>
2014-07-21 22:32:18 -0500

    Changed Version Number to 2

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.1.*")]
-[assembly: AssemblyFileVersion("1.1.1.5")]
+[assembly: AssemblyVersion("2.1.1.*")]
+[assembly: AssemblyFileVersion("2.1.1.5")]
 
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyCompany("HBCL")]
