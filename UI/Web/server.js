9537d740db7dd9bfaa0fad3382f439c265055a57
nudayabhanu-cci <nudayabhanu@CreativeCapsule.local>
2016-08-09 15:41:18 +0530

    Created Profile Page
    Profile Page can modify existing data once user has sucessfully logged
    in

--- UI/Web/server.js
+++ UI/Web/server.js
@@ -14,10 +14,10 @@ var express = require('express'),
 		console.log('Sucessfully connected to mongodb server');
 		
 		app.get('/',function(req,res){
-			res.render(__dirname+'/views/login',{errorMessage:""});
+			res.render(__dirname+'/views/login',{errorMessage:"",user:""});
 		});
 		app.get('/login.html',function(req,res){
-			res.render(__dirname+'/views/login',{errorMessage:""});
+			res.render(__dirname+'/views/login',{errorMessage:"",user:""});
 			
 		});
 		app.get('/registration.html',function(req,res){
@@ -31,43 +31,55 @@ var express = require('express'),
 			cursor.each(function(err,data){
 				if(err)
 				{	console.log("error!!");
-					res.render(__dirname+'/views/registration',{});
+					res.render(__dirname+'/views/login',{errorMessage:"",user:""});
 				}
 				else if(data)
 				{	if(password == data.password)
-						{	res.render(__dirname+'/views/home',{});
+						{	res.render(__dirname+'/views/profilePage',{user:userName});
 						}
 					else
-						{	res.render(__dirname+'/views/login',{errorMessage:"Invalid password"});
+						{	res.render(__dirname+'/views/login',{errorMessage:"Invalid password",user:userName});
 						}
 					if(userName!=data.username)
-						{	res.render(__dirname+'/views/registration',{});
+						{	res.render(__dirname+'/views/login',{errorMessage:"Invalid Username & password",user:userName});
 						}
 				}
 				else
-					res.render(__dirname+'/views/registration',{});
+					res.render(__dirname+'/views/login',{errorMessage:"Invalid Username & password",user:userName});
 			});
 								
 		});
 		
 		app.post('/registrationdetails',function(req,res){
 			var username = req.body.email;
-			var password = req.body.password;
+			var password = req.body.password;			
+			
+			db.collection('employee').insertOne({'username':username,'password':password},function(err,r){
+				assert.equal(null,err);
+				console.log('Entry saved with _ID'+r.insertedId);
+			});
+			res.render(__dirname+'/views/login',{user:username,errorMessage:""});
+		});
+
+		app.post('/update',function(req,res){
+			var username = req.body.email;
 			var firstName = req.body.firstName;
 			var middleName = req.body.middleName;
 			var lastName = req.body.lastName;
 			var dateOfBirth = req.body.dateOfBirth;
 			var dateOfJoining = req.body.dateOfJoining;
 			var designation = req.body.designation;
+			var team = req.body.teamName;
+			var skill = req.body.skills;
 			
-			db.collection('employee').insertOne({'username':username,'password':password,'firstName':firstName,'middleName':middleName,'lastName':lastName,'dateOfBirth':dateOfBirth,'dateOfJoining':dateOfJoining,'designation':designation},function(err,r){
+			db.collection('employee').updateOne({'username':username},{$set:{'firstName':firstName,'middleName':middleName,'lastName':lastName,'dateOfBirth':dateOfBirth,'dateOfJoining':dateOfJoining,'designation':designation,'team':team,'skils':skill}},function(err,r){
 				assert.equal(null,err);
-				console.log('Entry saved with _ID'+r.insertedId);
 			});
-			res.render(__dirname+'/views/login');
+
+			res.render(__dirname+'/views/login',{user:"",errorMessage:""});
 		});
 		
-		var server = app.listen(8001,function(){
+		var server = app.listen(8000,function(){
 			var port = server.address().port;
 			console.log('express app running on port %s',port);
 		});
