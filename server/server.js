2868219c29f1a3cf6967fe077d699365ab6a50b2
nudayabhanu-cci <nikhil.udayabhanu@creativecapsule.com>
2016-08-30 15:47:09 +0530

    [NUD]-Updated Server.js
    Added headers for token

--- server/server.js
+++ server/server.js
@@ -1,56 +1,61 @@
 var express = require('express'),
-	MongoClient = require('mongodb').MongoClient,
-	assert = require('assert'),
-	bodyParser = require('body-parser'),
-	cors = require('express-cors'),
-	mongoose = require('mongoose');
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
+    bodyParser = require('body-parser'),
+    cors = require('express-cors'),
+    mongoose = require('mongoose');
 
 var app = express();
 var envConfig = require('./app/config/config.env.js');
+var port = require('./app/config/config.env.js').port;
 
 var user = require('./app/routes/user');
 
-	//Setting up the app
-	app.set('view engine','ejs');	//setting view engine as ejs
-	app.use(express.static('app'));	//setting up middleware to serve static files
-	app.use(bodyParser.urlencoded({extended:true}));
-	app.use(bodyParser.json());
-	app.use(cors({
-		allowedOrigins:['http://localhost:3000'],
-		//headers:[]
-	}));
+//Setting up the app
+app.set('view engine', 'ejs'); //setting view engine as ejs
+app.use(express.static('app')); //setting up middleware to serve static files
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+    allowedOrigins: ['http://localhost:3000'],
+    //headers:[]
+    headers:[
+        'x-access-token', 'Content-Type',
+       'Authorization',  'Bearer'
+       ]
+}));
 
 //MongoClient.connect('mongodb:.....'function(err,db)){		};	
 
-mongoose.connect(envConfig.mongoUrl,function(err){
-	if(err) mongooseLog('Mongoose Error:'+ err);
+mongoose.connect(envConfig.mongoUrl, function(err) {
+    if (err) mongooseLog('Mongoose Error:' + err);
 });
 
 mongoose.connection
-    .on('connected', function () {
+    .on('connected', function() {
         mongooseLog('Connection open to ' + envConfig.mongoUrl);
     })
-    .on('error',function (err) {
+    .on('error', function(err) {
         mongooseLog('Connection error: ' + err);
     })
-    .on('disconnected', function () {
+    .on('disconnected', function() {
         mongooseLog('Connection disconnected');
     });
 
 
 function mongooseLog(data) {
-  return console.log(data);
+    return console.log(data);
 }
 
 app.use('/user', user);
 
-app.get('/',function(req,res){
-	res.send("Server waiting for requests on port 9000");
+app.get('/', function(req, res) {
+    res.send("Server waiting for requests on port " + port);
 });
 //app.set('port', 9000);
 
-app.listen(9000, function () {
-    console.log( "Express server listening on port 9000" );
+app.listen(9000, function() {
+    console.log("Express server listening on port " + port);
 });
 
 
