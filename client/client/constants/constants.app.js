665575ca300cc9c9a52c9265e05f5581ea3ea505
dshet-cci <darshani.shet@creativecapsule.com>
2016-08-30 16:16:36 +0530

    [DSH] : Redirect to login page upon new user registration
    * Show sweetalert after new user registration
    * Redirect to login page
    * Worked on proper folder structure for JS and CSS files

--- client/client/constants/constants.app.js
+++ client/client/constants/constants.app.js
@@ -12,7 +12,8 @@ var constants = {
     dateFormat: 'mm-dd-yy',
     msgUsernamePasswordFailure: 'Incorrect username or password!',
     msgPasswordMatchFailure: 'Incorrect password match!',
-    msgUsernameRegisterFailure: 'Username is already registered! Please choose different Username.'
+    msgUsernameRegisterFailure: 'Username is already registered! Please choose different Username.',
+    msgUserRegisteredSuccess: 'User registered successfully!'
 
 };
 angular.module('employeeApp').constant('constants', constants);
--- client/client/controllers/registrationController.js
+++ client/client/controllers/registrationController.js
@@ -1,34 +1,51 @@
 (function() {
-    function registrationController($scope, authentication, $location, constants) {
+    function registrationController($scope, authentication, $location, constants, $window) {
         $scope.button = true;
 
 
         $scope.register = function() {
             var password1 = $scope.credentials.password;
             var password2 = $scope.credentials.confirmPassword;
-            var credentials = $scope.credentials;            
+            var credentials = $scope.credentials;
 
-            if (password1 != password2) {                
+            if (password1 != password2) {
                 $scope.errorMessage = constants.msgPasswordMatchFailure;
 
             } else {
                 $scope.errorMessage = "";
 
-                authentication.register(credentials)
+                authentication
+                    .register(credentials)
                     .error(function(err) {
                         console.log("error" + err.message);
                         $scope.usernameErrorMessage = constants.msgUsernameRegisterFailure;
 
                     })
                     .success(function(data) {
+
+                        swal({
+                            title: "",
+                            text: constants.msgUserRegisteredSuccess,
+                            type: "info",
+                            html: true,
+                            showCancelButton: false,
+                            closeOnConfirm: false,
+                            showLoaderOnConfirm: true,
+                        }, function() {
+                            setTimeout(function() {
+                                $window.location.reload();
+                                window.location.href = '#/login';
+                            }, 500);
+                        });
+
                         console.log("success" + JSON.stringify(data));
-                        $location.path('profile');
+                        //$location.path('profile');
                     });
             }
         }
 
 
     };
-    registrationController.$inject = ['$scope', 'authentication', '$location', 'constants'];
+    registrationController.$inject = ['$scope', 'authentication', '$location', 'constants', '$window'];
     angular.module("employeeApp").controller("registrationController", registrationController);
 }());
