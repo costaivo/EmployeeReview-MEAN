9b28f41eb2555eee8e200216bd9430836cff568b
nudayabhanu-cci <nikhil.udayabhanu@creativecapsule.com>
2016-08-30 16:00:51 +0530

    [NUD]-Updated conifg.env.js
    port number is added in config.env.js

--- server/app/config/config.env.js
+++ server/app/config/config.env.js
@@ -1,4 +1,5 @@
-module.exports={
-	//ports:[27017],
-	mongoUrl:"mongodb://localhost:27017/myapp",
+module.exports = {
+    //ports:[27017],
+    mongoUrl: "mongodb://localhost:27017/EmployeeReview",
+    port: 9000,
 };
