3858451d82ef9e7eadf02743a04841d7a41f1ac1
dshet-cci <darshani.shet@creativecapsule.com>
2016-09-13 15:41:27 +0530

    [DSH]: Implemented footer
    * Implemented footer to display app version
    * Optimised code for side bar and nav bar

--- client/client/controllers/homeController.js
+++ client/client/controllers/homeController.js
@@ -1,8 +1,11 @@
-(function(){
-	function home($scope){
-		$scope.message = "Welcome";
-	};
-	home.$inject = ['$scope'];
-	angular.module("employeeApp").controller("homeController",home);
-	home.$inject = ['$scope'];
+(function() {
+    function home($scope, constants) {
+        $scope.message = "Welcome";
+        $scope.appVersion = 'version ' + constants.minor + '.' + constants.major + '.' + constants.build;
+    };
+    home.$inject = ['$scope', 'constants'];
+    angular.module("employeeApp").controller("homeController", home);
+
+
+
 }());
